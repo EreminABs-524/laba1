a43ed161b1363addfda76a7c91c08706784e85da
IvanABs-524 <ivantapkin336@gmail.com>
2026-03-20 09:50:43 +0700

    Добавление функции для окружности

---
+++ circleFunction.cpp
@@ -0,0 +1,9 @@
+int circleLength(int radius){
+	return radius*2*pi;
+}
+int circleArea(int radius){
+	return pi*radius*radius;
+}
+int circleSector(int radius, int degree){
+	return (radius*radius*degree*pi)/360;
+}
