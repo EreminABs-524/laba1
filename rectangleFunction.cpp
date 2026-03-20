d06341417cd054fc445cddafb189efe42eca0994
IvanABs-524 <ivantapkin336@gmail.com>
2026-03-20 09:41:03 +0700

    Файл с функциями для прямоугольника

---
+++ rectangleFunction.cpp
@@ -0,0 +1,10 @@
+#include <cmath>
+int rectanglePerimeter(int firstEdge, int secondEdge){
+	return firstEdge*2+secondEdge*2;
+}
+int rectangleArea(int firstEdge, int secondEdge){
+	return firstEdge*secondEdge;
+}
+int rectangleDiagonal(int firstEdge, int secondEdge){
+	return  sqrt(firstEdge*firstEdge+secondEdge*secondEdge);
+}
